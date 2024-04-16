6df91c1609cca1dcb7b41d4799f01b2bb3422788
tokiya <tokiya.syouta@icloud.com>
2024-04-17 00:20:27 +0900

    押したレーンの発光処理追加

--- Assets/Script/Lane/LaneController.cs
+++ Assets/Script/Lane/LaneController.cs
@@ -7,25 +7,49 @@ using UnityEngine;
 /// </summary>
 public class LaneController : MonoBehaviour
 {
-    [SerializeField] GameObject redLane;
-    [SerializeField] GameObject greenLane;
-    [SerializeField] GameObject blueLane;
-
-    Renderer redLaneRenderer;
-
-    void Start()
-    {
-        redLaneRenderer = redLane.GetComponent<Renderer>();
-    }
+    // 各レーンのマテリアル
+    [SerializeField] Material redLaneMaterial;
+    [SerializeField] Material greenLaneMaterial;
+    [SerializeField] Material blueLaneMaterial;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F)) 
-        {
-            //redLaneRenderer.material.
-        }
+        // キー入力時に応じたレーンの発光
+        LuminescenceLane();
     }
 
-
+    /// <summary>
+    /// レーンの発光
+    /// </summary>
+    void LuminescenceLane()
+    {
+        // 赤レーン
+        if (Input.GetKey(KeyCode.F))
+        {
+            redLaneMaterial.SetColor("_EmissionColor", Color.red);
+        }
+        else
+        {
+            redLaneMaterial.SetColor("_EmissionColor", Color.black);
+        }
+        // 緑レーン
+        if (Input.GetKey(KeyCode.J))
+        {
+            greenLaneMaterial.SetColor("_EmissionColor", Color.green);
+        }
+        else
+        {
+            greenLaneMaterial.SetColor("_EmissionColor", Color.black);
+        }
+        // 青レーン
+        if (Input.GetKey(KeyCode.K))
+        {
+            blueLaneMaterial.SetColor("_EmissionColor", Color.blue);
+        }
+        else
+        {
+            blueLaneMaterial.SetColor("_EmissionColor", Color.black);
+        }
+    }
 }
